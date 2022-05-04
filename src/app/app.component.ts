fd6f772c40c9b1d79fccbf595b9ec006c4f78c56
gemdevrats <devvrat.sharma@geminisoloution.com>
2022-05-04 13:14:12 +0530

    edit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+
+  res : any = {};
+  ngOnInit(): void {
+    axios('https://newsapi.org/v2/everything?q=Top%20news&from=2022-04-04&sortBy=publishedAt&language=en&apiKey=e1f644af21fd48d7a4c41967d83f41df')
+    .then(response => {
+      this.res = response.data.articles;
+    }).then(e=>{
+      console.log(e);
+    });
+  }
+  
+  topic(value:string){
+    console.log(value);
+    axios('https://newsapi.org/v2/everything?q='+ value + '&from=2022-04-04&sortBy=publishedAt&language=en&apiKey=e1f644af21fd48d7a4c41967d83f41df')
+    .then(response => {
+      console.log(response.data.articles);
+      this.res = response.data.articles;
+    }).then(e=>{
+      console.log(e);
+    });
+  }
+
   title = 'angular-forms-tutorial';
 }
